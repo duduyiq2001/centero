e1b7da3f04a9d2a5f630501bd4ba2e6e2265a46c
Tim Twigg <tim@twiggusa.com>
2023-11-17 12:23:43 -0800

    Work on Manager interface

--- lib/views/managerhome.dart
+++ lib/views/managerhome.dart
@@ -10,6 +10,9 @@ class ManagerHome extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    var showLeftPanel = useState(true);
+    var showRightPanel = useState(true);
+
     return Scaffold(
       bottomSheet: const Footer(),
       appBar: AppBar(
@@ -28,41 +31,233 @@ class ManagerHome extends HookWidget {
         actions: const <Widget>[],
       ),
       body: Container(
-        margin: EdgeInsets.all(5 * CenteroTheme.getValues(context).scaleFactor),
+        margin:
+            EdgeInsets.all(10 * CenteroTheme.getValues(context).scaleFactor),
         padding:
             EdgeInsets.only(bottom: CenteroTheme.getValues(context).footerSize),
         child: Row(
           children: <Widget>[
-            // tenant info section on the left
-            Expanded(
-              flex: 7,
-              child: Column(),
-            ),
-            // incoming call info section on the right
-            Expanded(
-              flex: 3,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black12,
-                  border: Border.all(width: 1, color: Colors.black38),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                        8 * CenteroTheme.getValues(context).scaleFactor),
-                  ),
-                ),
-                padding: EdgeInsets.all(
-                  15 * CenteroTheme.getValues(context).scaleFactor,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "Call Information",
-                      style: Theme.of(context).textTheme.headlineMedium,
+            // left panel
+            (showLeftPanel.value)
+                ? Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(
+                            10 * CenteroTheme.getValues(context).scaleFactor,
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              showLeftPanel.value = false;
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(
+                                  Icons.arrow_left_rounded,
+                                  size: 40 *
+                                      CenteroTheme.getValues(context)
+                                          .scaleFactor,
+                                ),
+                                Text(
+                                  "Hide",
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 1.5,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                border:
+                                    Border.all(width: 1, color: Colors.black38),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    8 *
+                                        CenteroTheme.getValues(context)
+                                            .scaleFactor,
+                                  ),
+                                ),
+                              ),
+                              padding: EdgeInsets.all(
+                                15 *
+                                    CenteroTheme.getValues(context).scaleFactor,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Google Map",
+                                  style: CenteroTheme.getTheme(context)
+                                      .textTheme
+                                      .headlineMedium,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(
+                            20 * CenteroTheme.getValues(context).scaleFactor,
+                          ),
+                          child: Text(
+                            "1409 Cherry Lane, Los Angeles, CA 90277",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ),
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 1.5,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                border:
+                                    Border.all(width: 1, color: Colors.black38),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    8 *
+                                        CenteroTheme.getValues(context)
+                                            .scaleFactor,
+                                  ),
+                                ),
+                              ),
+                              padding: EdgeInsets.all(
+                                15 *
+                                    CenteroTheme.getValues(context).scaleFactor,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Floor Plan",
+                                  style: CenteroTheme.getTheme(context)
+                                      .textTheme
+                                      .headlineMedium,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(
+                            20 * CenteroTheme.getValues(context).scaleFactor,
+                          ),
+                          child: Text(
+                            "Unit 2051A\n2BR 1BA \"Colonial\"",
+                            style: Theme.of(context).textTheme.bodyLarge,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
+                  )
+                : Container(
+                    alignment: Alignment.centerLeft,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_right_rounded,
+                        size: 40 * CenteroTheme.getValues(context).scaleFactor,
+                      ),
+                      onPressed: () {
+                        showLeftPanel.value = true;
+                      },
+                    ),
+                  ),
+            // center panel
+            Expanded(
+              flex: 5,
+              child: Container(),
             ),
+            // right panel
+            (showRightPanel.value)
+                ? Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(
+                            10 * CenteroTheme.getValues(context).scaleFactor,
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              showRightPanel.value = false;
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "Hide",
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                                Icon(
+                                  Icons.arrow_right_rounded,
+                                  size: 40 *
+                                      CenteroTheme.getValues(context)
+                                          .scaleFactor,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                border:
+                                    Border.all(width: 1, color: Colors.black38),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    8 *
+                                        CenteroTheme.getValues(context)
+                                            .scaleFactor,
+                                  ),
+                                ),
+                              ),
+                              padding: EdgeInsets.all(
+                                15 *
+                                    CenteroTheme.getValues(context).scaleFactor,
+                              ),
+                              margin: EdgeInsets.only(
+                                right: 20 *
+                                    CenteroTheme.getValues(context).scaleFactor,
+                                left: 20 *
+                                    CenteroTheme.getValues(context).scaleFactor,
+                                bottom: 20 *
+                                    CenteroTheme.getValues(context).scaleFactor,
+                              ),
+                              child: Column(
+                                children: <Widget>[
+                                  Text(
+                                    "Profile",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_left_rounded,
+                        size: 40 * CenteroTheme.getValues(context).scaleFactor,
+                      ),
+                      onPressed: () {
+                        showRightPanel.value = true;
+                      },
+                    ),
+                  ),
           ],
         ),
       ),
