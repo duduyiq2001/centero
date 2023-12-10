c41a0fcd6d36e618fccff1c5c81c7ee534179b0b
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2023-12-10 02:24:13 -0800

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -53,11 +53,4 @@ class DefaultFirebaseOptions {
   }
 }
 
-const FirebaseOptions web = FirebaseOptions(
-    apiKey: "AIzaSyCNJs_VyFxI7OFAaNxOIoL4KEPtSF0mKUc",
-    authDomain: "centerobackend.firebaseapp.com",
-    projectId: "centerobackend",
-    storageBucket: "centerobackend.appspot.com",
-    messagingSenderId: "780428703318",
-    appId: "1:780428703318:web:670d5ac13609f2e9fb6df7",
-    measurementId: "G-VDDP4KG3DZ");
+
